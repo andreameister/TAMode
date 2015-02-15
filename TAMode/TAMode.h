a16338dbc0227a0c244d551098f7b3c9edab9f84
Aaron Meyer <aminute@gmail.com>
2015-02-15 10:38:05 -0500

    Initial Commit

---
+++ TAMode/TAMode.h
@@ -0,0 +1,23 @@
+/*
+ *  TAMode.h
+ *  TAMode
+ *
+ *  Created by Aaron Meyer on 2/15/15.
+ *  Copyright (c) 2015 Aaron Meyer. All rights reserved.
+ *
+ */
+
+#ifndef TAMode_
+#define TAMode_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class TAMode
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
